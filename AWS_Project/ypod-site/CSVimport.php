81ec38c4ea70c7ed150da5140169ec19af9f4e08
Drew Meyers <drew.meyers@colorado.edu>
2016-03-31 18:24:20 +0000

    Update CSVimport.php
    Added .csv format verification. Now only files matching standard YPOD file format will be able to be uploaded into DB.

--- AWS_Project/ypod-site/CSVimport.php
+++ AWS_Project/ypod-site/CSVimport.php
@@ -1,5 +1,5 @@
-<?php include("auth.php"); //include auth.php file on all secure pages ?>
 <?PHP
+include("auth.php"); //include auth.php file on all secure pages
 $uploaded = $_FILES['csv']['name'];
 if ($_FILES[csv][size] > 0) { 
 $hostname = "localhost";
@@ -17,6 +17,29 @@ $selectdatabase = mysql_select_db("ypod",$dbhandle)
 $uploaded = $_FILES['csv']['name'];
 move_uploaded_file($_FILES["csv"]["tmp_name"],
             "C:\Bitnami\wampstack-5.6.19-0\apache2\htdocs\login-site/" . $_FILES["csv"]["name"]);
+
+$requiredHeaders = array("Rtc Date","Rtc Time","BMP Temp(C)",
+"BMP Pres(mb)", "SHT25 Temp(C)", "SHT25 Humidity", "CO2(ADC VAL)",
+"Wind Speed(mph)", "Wind Direction(deg)", "Quad-Aux-1(microVolts)",
+"Quad-Main-1(microVolts)", "Quad-Aux-2(microVolts)", "Quad-Main-2(microVolts)",
+"Quad-Aux-3(microVolts)", "Quad-Main-3(microVolts)", "Quad-Aux-4(microVolts)",
+"Quad-Main-4(microVolts)", "Fig 210 Heat(milliVolts)", "Fig 210 Sens(milliVolts)",
+"Fig 280 Heat(milliVolts)", "Fig 280 Sens(milliVolts)", "BL Moccon sens(milliVolts)",
+"ADC2 Channel-2(empty)", "E2VO3 Heat(milliVolts)", "E2VO3 Sens(milliVolts)", "GPS Date", "GPS Time(UTC)",
+"Latitude", "Longitude", "Fix Quality", "Altitude(meters above sea level)", "Statellites"); //headers we expect
+
+$f = fopen($uploaded, 'r');
+$firstLine = fgets($f); //get first line of csv file
+//fclose($f)
+
+
+$foundHeaders = str_getcsv(trim($firstLine), ',', '"'); //parse to array
+
+if ($foundHeaders !== $requiredHeaders) {
+   die('.csv file does not match required format. Make sure youre uploading a valid POD file.<br>' . mysql_error());
+}
+
+else if($foundHeaders == $requiredHeaders){
 if (!empty($uploaded)) {
 	$query = "LOAD DATA LOCAL INFILE '" . $uploaded . "' INTO TABLE AQIQ_raw FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES ";
 	mysql_query($query) 
@@ -25,6 +48,7 @@ if (!empty($uploaded)) {
 	if (is_file($uploaded)) {
 		unlink($uploaded );
 	}
+}
 }
 }
 ?>
