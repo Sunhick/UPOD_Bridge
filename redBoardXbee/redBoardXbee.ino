58f06b728b13f2c43272d166ef2c1c2e0814e056
DrewMeyersCUboulder <aqiq@AQIQ.(none)>
2016-07-24 13:00:28 -0600

    refined redboard firmware

--- redBoardXbee/redBoardXbee.ino
+++ redBoardXbee/redBoardXbee.ino
@@ -1,28 +1,27 @@
 #include <SPI.h>
 #include <Wire.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <SdFat.h>
 #include <SFE_BMP180.h>
 #include <RTC_DS3231.h>
 #include <mcp3424.h>
 #include <Adafruit_ADS1015.h>
+#include <XBee.h>
 
-#define SerialStream 0
-#define GPS_used 1
+#define SerialStream 0 //if SerialStream used, XBee can not be.
+#define XBee_used 1 //if XBee used, SerialStream can not be.
 #define MetStation 1
 #define QuadStat  1 //auxillary 4-stat array, uses 2 MCP3424s
 
 /*****UPOD model indicator. Modify the 4th and 5th character to denote which UPOD you are using.*****/
-String ypodID = "YPODA1";
-String fileName;
-
-word setFileName = 1;
+char ypodID[7] = "YPODA1";
 
 const word chipSelect = 4;
 String delimiter = ",";
 
-#if GPS_used
-SoftwareSerial GPS(8, 9);
+#if XBee_used
+XBee xbee = XBee();
+XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40c84aab);
 #endif
 
 RTC_DS3231 RTC;
@@ -49,14 +48,6 @@ SFE_BMP180 BMP;
 //SHT2 Temp and Humidity Variables
 unsigned int temperature_board, humidity_board;
 
-#if GPS_used
-//Interrupt for GPS
-void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
-uint32_t timer = millis();
-String gps_data;
-bool gps_available = false;
-#endif
-
 #if MetStation
 //Wind direction sensor(Potentiometer) on analog pin 0
 const byte WDIR = A0;
@@ -77,13 +68,12 @@ void wspeedIRQ()  {
 #endif
 
 void setup() {
-  #if SerialStream
   Serial.begin(9600);
+
+  #if XBee_used
+  xbee.setSerial(Serial);
   #endif
-  #if GPS_used
-  GPS.begin(4800);
-  #endif
-  
+
   Wire.begin();
   SPI.begin();
   RTC.begin();
@@ -93,11 +83,6 @@ void setup() {
   pinMode(chipSelect, OUTPUT);
   pinMode(10, OUTPUT);
 
-  DateTime now = RTC.now();
-  fileName = ypodID + "_" + String(now.year()) + "_" + String(now.month()) + "_" + String(now.day()) + ".txt";
-  char fileNameArray[fileName.length()+1];
-  fileName.toCharArray(fileNameArray, sizeof(fileNameArray)); //Well damn, that function is nice.
-
   while (!sd.begin(chipSelect)) {
     #if SerialStream
     Serial.println("insert sd card to begin");
@@ -106,65 +91,21 @@ void setup() {
   }
 
   digitalWrite(10, LOW);
-/*  
-  //Open file and write header
-    file.open(fileNameArray, O_CREAT | O_APPEND | O_WRITE);
-    file.println("ID,Date,Time,bmpTemp,bmpPres,shtTemp,shtHum,CO2,windSpeed,windDir,Quad_Aux1,Quad_Main1,Quad_Aux2,Quad_Main2,Quad_Aux3,Quad_Main3,Quad_Aux4,Quad_Main4,Fig210_Heat,Fig210_Sens,Fig280_Heat,Fig280_Sens,blMocon,NULL,E2VO3_Heat,E2VO3_Sens,GPS");    // Print header to file
-    file.close();    // close file - very important
-*/
-  #if QuadStat
+
+#if QuadStat
   alpha_one.GetAddress('G', 'F'); //user defined address for the alphasense pstat array (4-stat)
   alpha_two.GetAddress('H', 'H') ;
-  #endif
-  
-  #if MetStation
-  attachInterrupt(4, wspeedIRQ, FALLING); //anemometer reed switch on pin 7--> interrupt# 4
-  #endif
-  
-  #if GPS_used
-  useInterrupt(true);
-  GPS.println("$PTNLSNM,0021,02"); //Address for ZDA output (UTC and date, no coordinates).
-  //GPS.println("$PTNLSNM,0001,02"); //Address for GGA output (coordinates, no date or time). 
-  //GPS.println("$PTNLSNM,0101,02");//Address for RMC output (UTC and coordinates, no date!!).
-    
-  delay(500);
-  GPS.println("$PTNLSNM,0021,02"); //Address for ZDA output (UTC and date, no coordinates).
-  //GPS.println("$PTNLSNM,0001,02");//Address for GGA output (coordinates, no time or date). 
-  //GPS.println("$PTNLSNM,0101,02");//Address for RMC output (UTC and coordinates, no date!!).
-  #endif
-}
-
-#if GPS_used
-void useInterrupt(boolean v) {
-  if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-  } else {
-    // do not call the interrupt function COMPA anymore
-    TIMSK0 &= ~_BV(OCIE0A);
-  }
-}
-
-// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-SIGNAL(TIMER0_COMPA_vect) {
-  if (GPS.available()) {
-    char c = GPS.read();
-    //    Serial.print(c);
-    if (!gps_available) {
-      gps_data += c;
-      if (c == '\n') gps_available = true;
-    }
-  }
-}
 #endif
 
-void loop() {
+#if MetStation
+  attachInterrupt(4, wspeedIRQ, FALLING); //anemometer reed switch on pin 7--> interrupt# 4
+#endif
+}
 
+void loop() {
   String data;
-  char fileNameArray[fileName.length()+1];
-  fileName.toCharArray(fileNameArray, sizeof(fileNameArray));
+
+  //Get time from RTC
   DateTime now = RTC.now();
 
   //Get SHT data
@@ -196,72 +137,65 @@ void loop() {
     T = -99;
     P = -99;
   }
-  
-  data += ypodID + delimiter + String(now.year()) + "-" + String(now.month()) + "-" + String(now.day()) + delimiter + String(now.hour()) + ":" + String(now.minute()) + ":" +String(now.second()) + delimiter + T + delimiter + P + delimiter +
+
+  data += String(ypodID) + delimiter + String(now.unixtime()) + delimiter + T + delimiter + P + delimiter +
           temperature_SHT + delimiter + humidity_SHT + delimiter +
           String(getS300CO2()) + delimiter;
-  
+
   #if MetStation
   data += String(get_wind_speed()) + delimiter + String(analogRead(A0)) + delimiter;
   #else if
   data += delimiter + delimiter;
   #endif
-  
+
   //Read ADCs on-board and on-Quadstat
   for (int i = 1; i <= 16; i++) {
-    if (i <= 4) data += 
-    #if QuadStat
-    alpha_one.GetValue(i) + 
-    #endif
-    delimiter;
-    
-    else if (i <= 8) data += 
-    #if QuadStat
-      alpha_two.GetValue(i - 4) + 
-    #endif 
-    delimiter;
+    if (i <= 4) data +=
+        #if QuadStat
+        alpha_one.GetValue(i) +
+        #endif
+        delimiter;
+
+    else if (i <= 8) data +=
+        #if QuadStat
+        alpha_two.GetValue(i - 4) +
+        #endif
+        delimiter;
     else if (i <= 12) data += ads1.readADC_SingleEnded(i - 9) + delimiter;
-    else if (i <= 16) data += ads2.readADC_SingleEnded(i - 13) + delimiter;
+    else if (i < 16) data += ads2.readADC_SingleEnded(i - 13) + delimiter; // Normally <= 16, since gps usually comes after this.
+    else if (i == 16) data += ads2.readADC_SingleEnded(i - 13); //Since no GPS, we'll have this as the last value, no delimiter after.
   }
 
-  #if GPS_used
-  //Get GPS data
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis())  timer = millis();
-  if (millis() - timer > 2000) {
-    timer = millis();
-    if (gps_available) {
-      data += gps_data;
-      gps_data = "";
-      gps_available = false;
-    }
+  while (!sd.begin(chipSelect)) {
+    #if SerialStream
+    Serial.println("error in loop");
+    #endif
+    sd.begin(chipSelect);
   }
-  data.replace("\n","");
-  data.replace("\r",""); // GPS adds carriage return, newline. We need to strip it from data before printing to file.
-  #endif
-  
-  while(!sd.begin(chipSelect)){
-      #if SerialStream
-      Serial.println("error in loop");
-      #endif
-      sd.begin(chipSelect);
-  }
-  
+
   digitalWrite(10, LOW);
 
   if (sd.begin(chipSelect))   // very important - reinitialize SD card on the SPI bus
   {
     digitalWrite(10, HIGH);
-    //file.open(fileNameArray, O_WRITE | O_AT_END);  // open file in write mode
-    file.open(fileNameArray, O_CREAT | O_APPEND | O_WRITE);
+    file.open(ypodID, O_CREAT | O_APPEND | O_WRITE);
     file.println(data);
     #if SerialStream
     Serial.println(data);
     #endif
     file.close();
     delay(250);
-    digitalWrite(10,LOW);
+    digitalWrite(10, LOW);
   }
+
+  #if XBee_used
+  //Prepare the character array (the buffer) with one extra character for the null terminator.
+  char payload[data.length() + 1];
+  //Copy data string into the character array.
+  data.toCharArray(payload, data.length() + 1);
+  ZBTxRequest zbTx = ZBTxRequest(addr64, (uint8_t *)payload, strlen(payload));
+  xbee.send(zbTx);
+  #endif
 
   //QuadStat takes ~11 seconds to sample. No need for delay in main loop
   #if QuadStat
@@ -270,15 +204,13 @@ void loop() {
   #endif
 }
 
-
-
 float getS300CO2()  {
   int i = 1;
   long reading;
   //float CO2val;
   wire_setup(0x31, 0x52, 7);
 
-  while (Wire.available()){
+  while (Wire.available()) {
     byte val = Wire.read();
     if (i == 2)  {
       reading = val;
@@ -319,7 +251,7 @@ unsigned int read_wire(byte cmd) {
 }
 #if MetStation
 //Returns the instataneous wind speed
-float get_wind_speed(){
+float get_wind_speed() {
   float deltaTime = millis() - lastWindCheck; //750ms
 
   deltaTime /= 1000.0; //Covert to seconds
