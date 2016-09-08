1be1c76552ef2f0e0490cdba8124caa2c5b328a1
Drew Meyers <drew.meyers@colorado.edu>
2016-09-08 11:39:51 -0600

    Update ds3231.ino
    Forced RTC to adjust time

--- redBoard/Libraries/RTClib/examples/ds3231/ds3231.ino
+++ redBoard/Libraries/RTClib/examples/ds3231/ds3231.ino
@@ -83,11 +83,11 @@ void setup () {
     Wire.begin();
     RTC.begin();
 
-    if (! RTC.isrunning()) {
+    //if (! RTC.isrunning()) {
       Serial.println("RTC is NOT running!");
       // following line sets the RTC to the date & time this sketch was compiled
       RTC.adjust(DateTime(__DATE__, __TIME__));
-    }
+    //}
   
     DateTime now = RTC.now();
     DateTime compiled = DateTime(__DATE__, __TIME__);
